bca0566360aeb773ff7a7d63413acba4f51799b0
Blechkelle <jeffrey.schuett@gmail.com>
2018-04-02 20:35:12 +0200

    improved songrequests layout

--- src/views/Songrequests/Songrequests.jsx
+++ src/views/Songrequests/Songrequests.jsx
@@ -3,19 +3,57 @@ import Paper from 'material-ui/Paper';
 import { FormattedMessage } from 'react-intl';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import PlayArrow from 'material-ui/svg-icons/av/play-arrow';
+import SkipArrow from 'material-ui/svg-icons/av/skip-next';
+import RevokeArrow from 'material-ui/svg-icons/av/skip-previous';
+import Volume from 'material-ui/svg-icons/av/volume-up';
 import { Container, Row, Col } from 'react-grid-system';
 import Slider from 'material-ui/Slider';
+import Popover from 'material-ui/Popover';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from 'material-ui/Table';
 
 import './_style.css';
 
 class Songrequests extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
+
+  handleClick = event => {
+    // This prevents ghost click.
+    event.preventDefault();
+
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget
+    });
+  };
+
+  handleRequestClose = () => {
+    this.setState({
+      open: false
+    });
+  };
+
   render() {
     return (
       <div className="pageContent">
         <h2 className="pageTitle">
           <FormattedMessage id="sidebar.songrequests" />
         </h2>
-        <Paper style={{ padding: '0px' }} className="pageContainer">
+        <Paper
+          style={{ padding: '0px', display: 'flex', marginBottom: '32px' }}
+          className="pageContainer"
+        >
           <div className="songrequestsCoverImage">
             <FloatingActionButton
               style={{
@@ -35,20 +73,282 @@ class Songrequests extends React.Component {
             />
           </div>
           <div className="songrequestsPlayer">
-            <h4 style={{ padding: '0px', margin: '0px' }}>
-              Bohemian Rhapsody <br />
+            <h4 style={{ padding: '0px', margin: '0px', color: '#525252' }}>
+              Bohemian Rhapsody{' '}
+              <FloatingActionButton
+                style={{
+                  marginTop: '0px',
+                  marginLeft: '10px',
+                  float: 'right',
+                  boxShadow: 'none'
+                }}
+                mini="true"
+                backgroundColor="#414358"
+              >
+                <SkipArrow />
+              </FloatingActionButton>
+              <FloatingActionButton
+                style={{
+                  marginTop: '0px',
+                  marginLeft: '10px',
+                  float: 'right',
+                  boxShadow: 'none'
+                }}
+                mini="true"
+                backgroundColor="#414358"
+              >
+                <RevokeArrow />
+              </FloatingActionButton>
+              <FloatingActionButton
+                onClick={this.handleClick}
+                style={{
+                  marginTop: '0px',
+                  marginLeft: '10px',
+                  float: 'right',
+                  boxShadow: 'none'
+                }}
+                mini="true"
+                backgroundColor="#00aeae"
+              >
+                <Volume />
+                <Popover
+                  open={this.state.open}
+                  animated="false"
+                  anchorEl={this.state.anchorEl}
+                  anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+                  targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+                  onRequestClose={this.handleRequestClose}
+                  style={{
+                    height: 'auto',
+                    width: 'auto'
+                  }}
+                >
+                  <Slider
+                    sliderStyle={{
+                      marginLeft: '15px',
+                      marginRight: '15px',
+                      marginTop: '11px',
+                      marginBottom: '11px',
+                      width: '150px'
+                    }}
+                    style={{ color: '#00aeae' }}
+                  />
+                </Popover>
+              </FloatingActionButton>
+              <br />
               <small>Queen</small>
+              <br />
+              <em style={{ fontSize: '12px', fontWeight: 'normal' }}>
+                Requestet von <b>John Doe</b>
+              </em>
             </h4>
             <Slider
-              sliderStyle={{ marginBottom: '0px' }}
+              sliderStyle={{ marginBottom: '0px', marginTop: '10px' }}
               style={{ color: '#00aeae' }}
             />
-            <div className="songrequestsInfo">
+            <div
+              className="songrequestsInfo"
+              style={{ color: '#525252', fontWeight: 'bold', fontSize: '14px' }}
+            >
               <span className="leftTime">00:00</span>
               <span className="rightTime">13:37</span>
             </div>
           </div>
         </Paper>
+        <Table>
+          <TableHeader
+            className="songrequestsTableHead"
+            adjustForCheckbox={false}
+            displaySelectAll={false}
+            selectable={false}
+          >
+            <TableRow className="songrequestsTableRow">
+              <TableHeaderColumn className="songrequestsTableColumn">
+                ID
+              </TableHeaderColumn>
+              <TableHeaderColumn className="songrequestsTableColumn">
+                Titel
+              </TableHeaderColumn>
+              <TableHeaderColumn className="songrequestsTableColumn">
+                Kanal
+              </TableHeaderColumn>
+              <TableHeaderColumn className="songrequestsTableColumn">
+                Dauer
+              </TableHeaderColumn>
+              <TableHeaderColumn className="songrequestsTableColumn">
+                Requestet von
+              </TableHeaderColumn>
+              <TableHeaderColumn className="songrequestsTableColumn">
+                Aktionen
+              </TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody
+            className="songrequestsTableBody"
+            displayRowCheckbox={false}
+          >
+            <TableRow>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+            </TableRow>
+            <TableRow>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+            </TableRow>
+            <TableRow>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+            </TableRow>
+            <TableRow>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+            </TableRow>
+            <TableRow>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+            </TableRow>
+            <TableRow>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+            </TableRow>
+            <TableRow>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+            </TableRow>
+            <TableRow>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+              <TableRowColumn className="songrequestsTableColumnBody">
+                1'337
+              </TableRowColumn>
+            </TableRow>
+          </TableBody>
+        </Table>
       </div>
     );
   }
