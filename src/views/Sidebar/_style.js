69ab05eb37f5e4ed8db54f7705b817a1d6dc8a97
Blechkelle <jeffrey.schuett@gmail.com>
2018-07-26 21:28:14 +0200

    changed sidebar style

--- src/views/Sidebar/_style.js
+++ src/views/Sidebar/_style.js
@@ -14,13 +14,15 @@ export const getMenuItemStyle = () => ({
 export const getHeaderMenuItem = () => ({
   ...getMenuItemStyle(),
   backgroundColor: '#00aeae',
-  fontSize: 14,
+  fontSize: 18,
+  fontWeight: '600',
   lineHeight: '48px',
   color: '#fff',
   marginLeft: 0,
   padding: '0px 16px 0px 16px',
   minHeight: 48,
-  position: 'relative'
+  position: 'relative',
+  textAlign: 'center'
 });
 
 export const getActiveMenuItem = () => ({
