4e69daf76ed127bc385dada933cc7cc1063d2de5
Blechkelle <jeffrey.schuett@gmail.com>
2018-07-30 20:55:02 +0200

    + Addet Content to English translation File

---
+++ src/translations/en_en.js
@@ -0,0 +1,99 @@
+export default {
+  // General
+  'common.save': 'Speichern',
+
+  // Welcome
+  'welcome.headline': 'Willkommen bei Twasi',
+
+  // Sidebar
+  'sidebar.overview': 'Übersicht',
+  'sidebar.status': 'Status',
+  'sidebar.settings': 'Einstellungen',
+  'sidebar.profile': 'Profil',
+  'sidebar.plugins': 'Plugin Store',
+  'sidebar.fakechat': 'Fakechat',
+  'sidebar.commands': 'Befehle',
+  'sidebar.songrequests': 'Songrequests',
+  'sidebar.docs': 'Dokumentation',
+  'sidebar.logout': 'Ausloggen',
+  'sidebar.urlshortener': 'URL Shortener',
+
+  // Sidebar
+  'header.give_feedback': 'Feedback geben',
+
+  // fakechat
+  'fakechat.explanation':
+    'Im Fakechat kannst du alle Funktionen des Bots unabhängig deines Chats testen.',
+
+  // Overview
+  'overview.follower': 'Follower',
+  'overview.viewer': 'Zuschauer getrackt',
+  'overview.messages': 'Chatnachrichten getrackt',
+  'overview.streams': 'Streams getrackt',
+  'overview.laststream': 'Letzter Stream',
+  'overview.table_id': 'Stream ID',
+  'overview.table_duration': 'Dauer',
+  'overview.table_follower': 'Follower +',
+  'overview.table_views': 'Aufrufe +',
+  'overview.table_viewermax': 'Zuschauer Maximum',
+  'overview.table_average': 'Zuschauer Durchschnitt',
+
+  // Status
+  'status.started': 'Läuft',
+  'status.stopped': 'Gestoppt',
+  'status.manage': 'Verwalten',
+  'status.eventlog': 'Ereignislog',
+
+  // Plugins
+  'plugins.headline': 'Plugin Store',
+  'plugins.explanation':
+    'Plugins sind Erweiterungen für Twasi. Du bestimmst, welche Plugins du für deinen Persönlichen Bot installieren bzw. deinstallieren möchtest.',
+  'plugins.card_headline': 'Was sind Plugins?',
+  'plugins.version': 'Version',
+  'plugins.commands': 'Befehle',
+  'plugins.all_text':
+    'Hier findest du die gesamte Sammlung an Plugins, die für Twasi erstellt wurden.',
+  'plugins.free_text': 'Hier findest du alle Kostenlosen Plugins für Twasi.',
+  'plugins.paid_text':
+    'Hier findest du alle Kostenpflichtigen Plugins für Twasi. Durch den Kauf eines Kostenpflichtigen Plugins unterstützt du den Entwickler direkt.',
+  'plugins.all': 'Alle',
+  'plugins.free': 'Kostenlos',
+  'plugins.paid': 'Kostenpflichtig',
+
+  // Settings
+  'settings.unsaved': 'Ungespeicherte Änderungen',
+  'settings.selectLanguage': 'Wähle eine Sprache',
+
+  // Profil
+  'profile.your_data': 'Deine Twasi Daten',
+  'profile.your_data_subline': 'Hier findest du deine Twasi Daten.',
+  'profile.your_data_refreshbutton': 'Aktualisieren',
+  'profile.your_data_twitchname': 'Twitch Name',
+  'profile.your_data_twitchid': 'Twitch ID',
+  'profile.your_data_rank': 'Twasi Rang',
+  'profile.your_data_registerdate': 'Registriert seit',
+  'profile.your_data_delete': 'Löschen',
+  'profile.your_data_deletelink': 'Account Löschen',
+  'profile.badges': 'Abzeichen',
+  'profile.badges_subline':
+    'Hier kannst du dein Aussehen in Leaderboards anpassen. Du kannst bis zu 3 Badges gleichzeitig auswählen.',
+  'profile.badges_savebutton': 'Speichern',
+  'profile.badges_yourbadges': 'Deine Abzeichen',
+  'profile.social': 'Social Media',
+  'profile.social_subline':
+    'Hier kannst du deine Social Media Pages verlinken. Plugins können mit deiner Zustimmung auf bestimmte Daten der verbundenen Accounts zurückgreifen.',
+  'profile.social_connected_as': 'Verbunden als',
+  'profile.social_disconnect': 'Trennen',
+  'profile.social_permissions': 'Berechtigungen',
+  'profile.social_notconnected': 'Noch nicht verbunden',
+
+  // Commands
+  'commands.title': 'Befehle',
+  'commands.addcommand': 'Befehl Hinzufügen',
+
+  // AccountSwitch
+  'accountswitch.switch_account': 'Account wechseln',
+
+  // Public
+  'public.headline': 'Public'
+};
