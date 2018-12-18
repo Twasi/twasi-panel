050e64f963020cb5e2c143ee57ba169b047710d2
Blechkelle <jeffrey.schuett@gmail.com>
2018-12-18 08:54:44 +0100

    changed loaderimage to white logo

--- src/views/common/FullpageLoader/FullpageLoader.jsx
+++ src/views/common/FullpageLoader/FullpageLoader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './_style.css';
-import twasiImage from '../resources/twasi_anim_loading.gif';
+import twasiImage from '../resources/twasi_flat_white.svg';
 import twasiLoadingBar from '../resources/twasi_anim_loadingbar.gif';
 
 const FullpageLoader = () => (
