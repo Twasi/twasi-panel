cc29b8819809be9eefe852b3eee7ed359c996e79
Blechkelle <jeffrey.schuett@gmail.com>
2019-04-02 21:20:15 +0200

    Changed Markdown compiler to "markdown-to-jsx"

--- src/views/Plugins/Documentation.jsx
+++ src/views/Plugins/Documentation.jsx
@@ -6,8 +6,13 @@ import CardContent from '@material-ui/core/CardContent';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import { FormattedMessage } from 'react-intl';
-import ReactMarkdown from 'react-markdown/with-html';
+import Markdown from 'markdown-to-jsx';
 import DocumentationText from './documentationText.jsx';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 class Documentation extends React.Component {
 
@@ -27,7 +32,7 @@ class Documentation extends React.Component {
           <DialogContent>
             <Typography>
               <h3 className="pageContainerTitle">
-                <FormattedMessage id="plugins.documentation.headline" />
+                <FormattedMessage id="plugins.documentation.headline" /> Beispiel
               </h3>
               <small>
                 <FormattedMessage id="plugins.documentation.subheadline" />
@@ -36,7 +41,31 @@ class Documentation extends React.Component {
             <br /><br />
             <Card className="pluginCard">
               <CardContent className="pluginCardContent" style={{ paddingBottom: '10px' }}>
-                <ReactMarkdown source={DocumentationText} escapeHtml="true" />
+                <Markdown
+                  options={{
+                    overrides: {
+                        table: {
+                          component: Table,
+                        },
+                        thead: {
+                          component: TableHead,
+                          props: {
+                            className: 'TableRow',
+                          },
+                        },
+                        td: {
+                          component: TableCell,
+                        },
+                        th: {
+                          component: TableCell,
+                        },
+                        tr: {
+                          component: TableRow,
+                        },
+                      },
+                  }}>
+                  {DocumentationText}
+                </Markdown>
               </CardContent>
             </Card>
           </DialogContent>
--- src/views/Plugins/documentationText.jsx
+++ src/views/Plugins/documentationText.jsx
@@ -1,21 +1,14 @@
 export default `
 ### Beschreibung\n
-***\n
 Das Beispielplugin ermöglicht es dir, ein Beispiel zu sehen, welches auf keine weiteren Funktionen aufbaut.\n
 Es ist lediglich dafür da, dir zu zeigen, wie eine Mögliche Dokumentation für ein Plugin aussehen könnte.\n
 Dafür steht hier möglichst viel Unsinn, der nach Möglichkeit nicht in einer echten Dokumentation zu sehen sein sollte.\n
 Dies ist lediglich ein Grundlayout.\n
 
+### Befehle\n
 
-
-
-
-
-
-
-
-
-
-
-
+| Befehl | Alias | Parameter | Ausgabe |
+|--------|-------|-----------|---------|
+| !beispiel | !bsp | -       | Dies ist ein Beispiel |
+| !add | -  | parameter 1: **Befehl** parameter 2: **Ausgabe** | erstellt einen neuen Befehl |
 `
